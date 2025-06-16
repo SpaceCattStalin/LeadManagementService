e4d39002efa3512c6eda728767031bebdbdaf098
SpaceCattStalin <spacestalincyka@gmail.com>
2025-06-16 20:21:14 +0700

    Add SharedContracts

--- src/Application/UseCases/Commands/CreateBooking/CreateBookingCommand.cs
+++ src/Application/UseCases/Commands/CreateBooking/CreateBookingCommand.cs
@@ -8,10 +8,6 @@ namespace Application.UseCases.Commands.CreateBooking
         public string UserFullName { get; set; } = default!;
         public string UserEmail { get; set; } = default!;
         public string UserPhoneNumber { get; set; } = default!;
-        public string? InterestedCampus { get; set; }
-        public string? InterestedAcademicField { get; set; }
-        public string? InterestedSpecialization { get; set; }
-        public string? InterestedCourse { get; set; }
         public string Reason { get; set; } = default!;
     }
 }
